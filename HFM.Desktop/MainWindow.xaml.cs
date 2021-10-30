af0f6642b47dcb608893df725d92ccdb443e9801
Kirill Maizels <maizels.kirill@gmail.com>
2021-10-30 15:16:06 +0200

    Initial WPF project

---
+++ HFM.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace HFM.Desktop
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void ButtonAddName_Click(object sender, RoutedEventArgs e)
+    {
+      if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstDevices.Items.Contains(txtName.Text))
+      {
+        lstDevices.Items.Add(txtName.Text);
+        txtName.Clear();
+      }
+    }
+  }
+}
